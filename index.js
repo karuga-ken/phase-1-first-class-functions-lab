2543b6e126fe3dfef6afaec6857ef53529a19e8c
Karuga Mbugua <karugambugua@icloud.com>
2023-10-05 16:41:03 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(kenny){
+    return kenny.slice(0,2);
+}
+const returnLastTwoDrivers = function(kenny){
+    return kenny.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (tripleFare = 3 ){
+    return function (charges = 100){
+        return tripleFare * charges
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (a, returnedDrivers){
+    return returnedDrivers(a)
+}
